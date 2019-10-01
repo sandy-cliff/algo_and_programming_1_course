f8964d3e8dc3b1d783a32d622a499cb12ca1d2f9
punkproger <punkproger@gmail.com>
2019-10-01 22:04:31 +0300

    Updated code of second program of 4th lab

--- lab4/main_1.c
+++ lab4/main_1.c
@@ -2,30 +2,52 @@
 
 int main()
 {
-	float a,b = 17.00;
-	int res;
+	int a;
 	int contin = 1;
 
 	while (contin ==1)
 	{
-		printf("What time is it?\n");
-		scanf("%f", &a);
-		res = b - a;
+		printf("How many seconds are left?\n");
+		scanf("%d", &a);
 
-		if ( a > 17.00 ) 
+		switch (a)
 		{
-			printf("YOUR DAY IS OVER\n");
+			case 28800 :
+				printf("8 hours left\n");
+				break;
+			case 25200 ... 28799 :
+				printf("7 hours left\n");
+				break;
+			case 21600 ... 25199 :
+				printf("6 hours left\n");
+				break;
+			case 18000 ... 21599 :
+				printf("5 hours left\n");
+				break;
+			case 14400 ... 17999 :
+				printf("4 hours left\n");
+				break;
+			case 10800 ... 14399 :
+				printf("3 hours left\n");
+				break;
+			case 7200 ... 10799 :
+				printf("2 hours left\n");
+				break;
+			case 3600 ... 7199 :
+				printf("1 hours left\n");
+				break;
+			case 1 ... 3599 :
+				printf("less than an hour\n");
+				break;
+			case 0 :
+				printf("day is over\n");
+				break;
+			default :
+				printf("ERROR\n");
 		}
-		else if ( a < 9.00 )
-		{
-			printf("YOUR DAY HAS NOT STARTED\n");
-		}
-		else 
-		{
-			printf("%d hours left before the end of the working day\n", res);
-		}
-	printf("\nDo you want to continue?\nyes-1\nno-other meaning\n");
-	scanf("%d", &contin);
+
+		printf("\nDo you want to continue?\nyes-1\nno-other meaning\n");
+		scanf("%d", &contin);
 	}
 		return 0;
 }
